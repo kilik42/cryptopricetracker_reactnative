f39567fc2f2d91ff3285cb691686a7a512885591
kilik42 <marvinevins@yahoo.com>
2022-07-15 21:45:13 -0500

    working on list item

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,5 +1,5 @@
 // #rnfe
-import { View, Text, StyleSheet.Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const ListItem = () => {
@@ -10,16 +10,15 @@ const ListItem = () => {
         {/* LEFT SIDE */}
         <View style={styles.itemLeft}>
             <Image source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png'}} style = {styles.image}/>
-            <View style={styles.itemSubtitle}>
-                <Text>Ethereum </Text>
-                <Text>Eth</Text>
+            <View style={styles.titlesWrapper}>
+                <Text style = {styles.title}>Ethereum </Text>
+                <Text style = {styles.subtitle}>Eth</Text>
             </View>
         </View>
         {/* Rightside */}
         <View style={styles.itemRight}>
-            <Text style={styles.itemPercentage}>
-                +0.00% 
-            </Text>'
+                <Text style = {styles.title}>Ethereum </Text>
+                <Text style = {[styles.subtitle, {color: 'red'}]}>Eth</Text>
         </View> 
 
 
@@ -31,8 +30,14 @@ const ListItem = () => {
 const styles = StyleSheet.create({
 
     itemWrapper: {
-     
-
+     paddingHorizontal: 16,
+     marginTop: 24,
+     flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#A9ABB1',
+        
     },
     itemLeft: {
 
@@ -44,6 +49,15 @@ const styles = StyleSheet.create({
 
     },
     image: {
+
+    },
+    title: {
+
+    },
+    titlesWrapper: {
+
+    },
+    subtitle: {
 
     }
 }   )
